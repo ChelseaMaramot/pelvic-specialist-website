0ba2945c8489d0fb6bd4d41e2e9b00ca00e14cd0
ChelseaMaramot <gracechelsea76@yahoo.com>
2024-12-24 23:23:07 -0500

    template

--- my-app/src/Pages/Home.jsx
+++ my-app/src/Pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Navbar from '../Components/Navbar';
+import Hero from '../Components/Hero';
+import About from '../Components/About';
+import Contact from '../Components/Contact';
+import Footer from '../Components/Footer';
 
 export default function Home() {
   return (
     <div>
-        <h1>Welcome to homepage</h1>
+        <Navbar></Navbar>
+        <Hero></Hero>
+        <About></About>
+        <Contact></Contact>
+        <Footer></Footer>
     </div>
   )
 }
