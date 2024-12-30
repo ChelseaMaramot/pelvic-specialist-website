7d94c84bbddc36adaa8d323f6cb94271eae2eb7d
ChelseaMaramot <gracechelsea76@yahoo.com>
2024-12-29 19:51:44 -0500

    Responsive footer

--- my-app/src/Components/Footer.jsx
+++ my-app/src/Components/Footer.jsx
@@ -1,124 +1,110 @@
 import React from "react";
-import logo from "../Assets/logo.png"; // Import the logo
+import logo from "../Assets/logo.png";
+
+const footerStyles = {
+  position: "relative",
+  marginTop: "5em",
+};
+
+const curvedDivStyles = {
+  position: "absolute",
+  top: "-30px",
+  left: 0,
+  width: "100%",
+  height: "300px",
+  backgroundColor: "#000",
+  borderRadius: "10% 10% 0 0",
+};
+
+const footerContentStyles = {
+  backgroundColor: "#000",
+  color: "#fff",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  padding: "2em 3em",
+  flexWrap: "wrap",
+  position: "relative",
+};
+
+const sectionStyles = {
+  flex: "1",
+  minWidth: "150px",
+  textAlign: "center",
+};
+
+const infoSectionStyles = {
+  flex: "2",
+  minWidth: "250px",
+  textAlign: "left",
+  fontFamily: 'Montserrat, sans-serif',
+  fontWeight: 400,
+  marginLeft: '1em',
+  marginTop: '1em',
+  textAlign: {xs: 'center', sm:'left'}
+};
+
+const navStyles = {
+  flex: "1",
+  minWidth: "150px",
+  textAlign: {xs: 'center', sm:'left'}
+};
+
+const linkStyles = {
+  display: "block",
+  color: "#fff",
+  textDecoration: "none",
+  margin: "0.5em 0",
+  fontFamily: "Montserrat, sans-serif",
+  fontWeight: 400,
+};
+
+
+const paragraphStyles = {
+  fontSize: '0.9em'
+};
+
+const contactInfoStyles = {
+  margin: '0',
+};
+
 
 export default function Footer() {
   return (
-    <footer style={{ position: "relative", marginTop: "5em" }}>
-      {/* Curved Top */}
-      <div
-        style={{
-          position: "absolute",
-          top: "-30px",
-          left: 0,
-          width: "100%",
-          height: "300px",
-          backgroundColor: "#000",
-          borderRadius: "10% 10% 0 0",
-        }}
-      ></div>
+    <footer style={footerStyles}>
+      <div style={curvedDivStyles}></div>
 
-      <div
-        style={{
-          backgroundColor: "#000",
-          color: "#fff",
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: "2em 3em",
-          flexWrap: "wrap",
-          position: "relative",
-        }}
-      >
-        <div style={{ flex: "1", minWidth: "150px", textAlign: "center" }}>
-          <img
-            src={logo}
-            alt="Pearl Logo"
-            style={{ width: "150px" }}
-          />
+      <div style={footerContentStyles}>
+        <div style={sectionStyles}>
+          <img src={logo} alt="Pearl Logo" style={{  width: window.innerWidth <= 600 ? "100px" : "150px" }} />
         </div>
 
-        <div style={{ flex: "2", minWidth: "250px", textAlign: "left" }}>
-          <p style={{ margin: "0", fontFamily: "Montserrat, sans-serif", fontWeight: 400 }}>
-            Mountain Wellness Centre
-          </p>
-          <p style={{ margin: "1em 0", fontFamily: "Montserrat, sans-serif", fontWeight: 400 }}>
-            437 Concession St, Hamilton ON
-          </p>
-          <p style={{ margin: "1em 0", fontFamily: "Montserrat, sans-serif", fontWeight: 400 }}>
-            pearlpelvicphysio@gmail.com
-          </p>
-          <p style={{ margin: "1em 0", fontFamily: "Montserrat, sans-serif", fontWeight: 400 }}>
+        <div style={infoSectionStyles}>
+          <p style={contactInfoStyles}>Mountain Wellness Centre</p>
+          <p style={paragraphStyles}>437 Concession St, Hamilton ON</p>
+          <p style={paragraphStyles}>pearlpelvicphysio@gmail.com</p>
+          <p style={paragraphStyles}>
             NR: 905-3853338
             <br />
             Cell: 289-4890914
           </p>
         </div>
 
-        <div style={{ flex: "1", minWidth: "150px", textAlign: "left" }}>
+        <div style={navStyles}>
           <nav>
-            <a
-              href="#home"
-              style={{
-                display: "block",
-                color: "#fff",
-                textDecoration: "none",
-                margin: "0.5em 0",
-                fontFamily: "Montserrat, sans-serif",
-                fontWeight: 400,
-              }}
-            >
+            <a href="#home" style={linkStyles}>
               Home
             </a>
-            <a
-              href="#about"
-              style={{
-                display: "block",
-                color: "#fff",
-                textDecoration: "none",
-                margin: "0.5em 0",
-                fontFamily: "Montserrat, sans-serif",
-                fontWeight: 400,
-              }}
-            >
+            <a href="#about" style={linkStyles}>
               About
             </a>
-            <a
-              href="#services"
-              style={{
-                display: "block",
-                color: "#fff",
-                textDecoration: "none",
-                margin: "0.5em 0",
-                fontFamily: "Montserrat, sans-serif",
-                fontWeight: 400,
-              }}
-            >
+            <a href="#services" style={linkStyles}>
               Services
             </a>
-            <a
-              href="#location"
-              style={{
-                display: "block",
-                color: "#fff",
-                textDecoration: "none",
-                margin: "0.5em 0",
-                fontFamily: "Montserrat, sans-serif",
-                fontWeight: 400,
-              }}
-            >
+            <a href="#location" style={linkStyles}>
               Location
             </a>
-            <a
-              href="#contact"
-              style={{
-                display: "block",
-                color: "#fff",
-                textDecoration: "none",
-                margin: "0.5em 0",
-                fontFamily: "Montserrat, sans-serif",
-                fontWeight: 400,
-              }}
-            >
+            <a href="#contact" style={linkStyles}>
               Contact
             </a>
           </nav>
