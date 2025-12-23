9ac2213fea618f8516f2f5f4187ba564ac5413d8
Chelsea Maramot <61253723+ChelseaMaramot@users.noreply.github.com>
2025-12-23 14:33:03 -0500

    Revert "prod-20251223-order-change"

--- my-app/src/Components/Footer.jsx
+++ my-app/src/Components/Footer.jsx
@@ -76,7 +76,6 @@ export default function Footer() {
     { label: "About", id: "About" },
     { label: "Services", id: "Services" },
     { label: "FAQ", id: "FAQ" },
-    { label: "Testimonials", id: "Testimonials" },
     { label: "Contact", id: "Contact" },
   ];
 
